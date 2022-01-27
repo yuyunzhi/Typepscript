2bfd70269bfb8983104262191739058f45c6049b
ivesyu <ivesyu@tencent.com>
2022-01-27 16:30:38 +0800

    feat: update

--- ts-api/Omit.ts
+++ ts-api/Omit.ts
@@ -1,8 +1,9 @@
-import { InputHTMLAttributes } from "react";
+import React from "react";
 
 // 在第一个参数里的属性，剔除第二个参数里的所有属性
 type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
+// 逻辑为：先剔除 K 的值值值值 ，然后把联合类型 Exclude<keyof T, K> 变为 interface
 
 interface InputProps {
     disabled: boolean;
@@ -12,4 +13,4 @@ interface InputProps {
     id:number
 }
 
-type x = Omit<InputProps, "disabled">
+type xType = Omit<InputProps, "disabled">
--- ts-api/common-use.ts
+++ ts-api/common-use.ts
@@ -1,19 +1,68 @@
-import { ButtonHTMLAttributes} from 'react'
+import { ButtonHTMLAttributes, HTMLAttributes, ReactChild, CSSProperties, ReactElement} from 'react'
+
+
+// 1、对一个元素属性进行透传可以使用HTMLAttributes<HTMLElement>
+
+/**
+interface Props extends React.HTMLAttributes<HTMLElement> {}
+
+const Aside: FC<Props> = (props) => {
+  const { className, ...rest } = props;
+  return (
+    <div className={className} {...rest}>
+      {props.children}
+    </div>
+  );
+};
+
+ */
+
 
 
 // 原生元素的属性
 //  NativeXXX = XXXHTMLAttributes<HTMLElement>
 type NativeButton = ButtonHTMLAttributes<HTMLElement>;
 
-
-//T泛型 默认值 = {}
 interface DataSourceObject {
     value: string;
 }
 
+
+
+
+// 函数可以用 type
+type Renderer = (item: any) => void;
+// 联合类型用 type
 type DataSourceType<T = {}> = T & DataSourceObject;
+// 有几种值可以用  type 
+type MenuMode = "horizontal" | "vertical";
+
+
+// 递归,key value ,可以使用 interface
+interface SourceDataItem {
+    text: string;
+    children ?: SourceDataItem[];
+}
 
 
 
 
 
+// 一些react 特别的属性写法
+interface IReact {
+    style: CSSProperties
+    children: ReactChild
+    render: Renderer
+    map: { [key: string]: string[] };
+    buttons?: Array<ReactElement>;
+}
+
+const reactData: IReact = {
+    style: 'class',
+    children : null,
+    render:(item:number):void=>{},
+    map:{
+        's':['1','2'],
+        'name':['1','2,3']
+    }
+}
