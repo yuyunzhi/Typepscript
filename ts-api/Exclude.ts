a6f0aa2c345842e45b60a9997bb56d5bb82c1317
ivesyu <ivesyu@tencent.com>
2022-02-10 11:04:49 +0800

    feat: update exclude

--- ts-api/Exclude.ts
+++ ts-api/Exclude.ts
@@ -5,4 +5,15 @@ type Exclude<T, U> = T extends U ? never : T
 type IED = '1' | '2' | '3';
 type IEE = '4' | '2';
 
+
 type xExclude = Exclude<IED, IEE> 
+
+/** 
+ * T extends U ？ never : T
+ * T = [t1,t2,t3 ……]
+ * 对T做循环遍历
+ * t1 是否在 U
+ * t2 是否在 U
+ * t3 是否在 U 
+ * ……
+ */
--- ts-api/Omit.ts
+++ ts-api/Omit.ts
@@ -3,8 +3,6 @@ import React from "react";
 // 在第一个参数里的属性，剔除第二个参数里的所有属性
 type Omit<T, K extends keyof any> = Pick<T, Exclude<keyof T, K>>;
 
-// 逻辑为：先剔除 K 的值值值值 ，然后把联合类型 Exclude<keyof T, K> 变为 interface
-
 interface InputProps {
     disabled: boolean;
     onSelect:()=>void;
@@ -13,4 +11,8 @@ interface InputProps {
     id:number
 }
 
-type xType = Omit<InputProps, "disabled">
+// Omit是剔除，保留剩余的 等价于 Pick选出所要保留的
+let x : Omit<InputProps, "disabled">
+
+
+
